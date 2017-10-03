88f6b3f8eedba9ea52f905f2f68d7877ea5dd319
Igor Zibarev <zibarev.i@gmail.com>
2017-10-03 18:52:54 +0300

    Download with curl if possible

--- sh/install.sh
+++ sh/install.sh
@@ -25,6 +25,11 @@ fi
 mkdir -p "bin"
 mkdir -p "releases/v${version}"
 
-wget -q "${url}" -O "releases/v${version}.tar.gz"
+# Download with curl if possible.
+if [ -x "$(which curl 2>/dev/null)" ]; then
+    curl -sSL "${url}" -o "releases/v${version}.tar.gz"
+else
+    wget -q "${url}" -O "releases/v${version}.tar.gz"
+fi
 tar xzf "releases/v${version}.tar.gz" -C "releases/v${version}"
 mv "releases/v${version}/bin/helms3" "bin/helms3"
