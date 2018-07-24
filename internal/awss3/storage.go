f288404901320f406b02dca828bb24ffe6ff875b
Iurii Tymoshevskyi <itymoshe@cisco.com>
2018-07-24 16:53:23 +0300

    added AWS_S3_SSE env var to control server side encryption

--- internal/awss3/storage.go
+++ internal/awss3/storage.go
@@ -18,6 +18,12 @@ import (
 	"k8s.io/helm/pkg/chartutil"
 	"k8s.io/helm/pkg/proto/hapi/chart"
 	"k8s.io/helm/pkg/provenance"
+	"os"
+)
+
+const (
+	// selects serverside encryption for bucket
+	awsS3encryption = "AWS_S3_SSE"
 )
 
 var (
@@ -227,14 +233,13 @@ func (s *Storage) PutChart(ctx context.Context, uri string, r io.Reader, chartMe
 	if err != nil {
 		return "", err
 	}
-	sseAlg := "aws:kms"
 	result, err := s3manager.NewUploader(s.session).UploadWithContext(
 		ctx,
 		&s3manager.UploadInput{
 			Bucket:               aws.String(bucket),
 			Key:                  aws.String(key),
 			ACL:                  aws.String(acl),
-			ServerSideEncryption: aws.String(sseAlg),
+			ServerSideEncryption: aws.String(os.Getenv(awsS3encryption)),
 			Body:                 r,
 			Metadata: map[string]*string{
 				metaChartMetadata: aws.String(chartMeta),
@@ -260,14 +265,13 @@ func (s *Storage) PutIndex(ctx context.Context, uri string, acl string, r io.Rea
 	if err != nil {
 		return err
 	}
-	sseAlg := "aws:kms"
 	_, err = s3manager.NewUploader(s.session).UploadWithContext(
 		ctx,
 		&s3manager.UploadInput{
 			Bucket:               aws.String(bucket),
 			Key:                  aws.String(key),
 			ACL:                  aws.String(acl),
-			ServerSideEncryption: aws.String(sseAlg),
+			ServerSideEncryption: aws.String(os.Getenv(awsS3encryption)),
 			Body:                 r,
 		})
 	if err != nil {
