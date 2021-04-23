c21ba4ef61ee84b5002c0466c7976fcf34110646
Patrik Egyed <pregnor@cisco.com>
2021-04-23 12:00:42 +0200

    feat: added dynamic bucket region

--- internal/awsutil/session_test.go
+++ internal/awsutil/session_test.go
@@ -3,7 +3,62 @@ package awsutil
 import (
 	"os"
 	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/stretchr/testify/require"
 )
+
+func TestDynamicBucketRegion(t *testing.T) {
+	t.Parallel()
+
+	defaultSession, err := Session()
+	require.NoError(t, err)
+	defaultRegion := aws.StringValue(defaultSession.Config.Region)
+
+	testCases := []struct {
+		caseDescription      string
+		expectedBucketRegion string
+		inputS3URL           string
+	}{
+		{
+			caseDescription:      "existing S3 bucket URL with host only (no key) -> success",
+			expectedBucketRegion: "eu-central-1",
+			inputS3URL:           "s3://eu-test-bucket",
+		},
+		{
+			caseDescription:      "existing S3 bucket URL with key -> success",
+			expectedBucketRegion: "ap-southeast-2",
+			inputS3URL:           "s3://cn-test-bucket/charts/chart-0.1.2.tgz",
+		},
+		{
+			caseDescription:      "invalid URL -> failing URI parsing, no effect (default region)",
+			expectedBucketRegion: defaultRegion,
+			inputS3URL:           "://not/a/URL",
+		},
+		{
+			caseDescription:      "invalid S3 URL -> failing request, no effect (default region)",
+			expectedBucketRegion: defaultRegion,
+			inputS3URL:           "",
+		},
+		{
+			caseDescription:      "not existing S3 URL -> no region header, no effect (default region)",
+			expectedBucketRegion: defaultRegion,
+			inputS3URL:           "s3://not-an-s3-bucket-url",
+		},
+	}
+
+	for _, testCase := range testCases {
+		testCase := testCase
+
+		t.Run(testCase.caseDescription, func(t *testing.T) {
+			t.Parallel()
+
+			actualSession, err := Session(DynamicBucketRegion(testCase.inputS3URL))
+			require.NoError(t, err)
+			require.Equal(t, testCase.expectedBucketRegion, aws.StringValue(actualSession.Config.Region))
+		})
+	}
+}
 
 func TestSessionWithCustomEndpoint(t *testing.T) {
 	os.Setenv("AWS_ENDPOINT", "foobar:1234")
